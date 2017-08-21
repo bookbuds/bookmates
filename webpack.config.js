073566434673f7738cbe67184c6e45798ca13030
Matt J <unlrn@users.noreply.github.com>
2017-08-21 19:18:04 -0400

    added browser sync for pug files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports =
     {
@@ -18,7 +19,24 @@ module.exports =
         plugins: [
             new webpack.optimize.OccurrenceOrderPlugin(),
             new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoEmitOnErrorsPlugin()
+            new webpack.NoEmitOnErrorsPlugin(),
+            new BrowserSyncPlugin({
+                host: 'localhost',
+                port: '3001',
+                proxy: 'http://localhost:3000/',
+                files: [{
+                    match: [
+                        '**/*.pug'
+                    ],
+                    fn: function(event, file) {
+                        if (event === "change") {
+                            const bs = require('browser-sync').get('bs-webpack-plugin');
+                            bs.reload();
+                        }
+                    }
+                }]
+
+            },{ reload: false })
         ],
         module:
         {
