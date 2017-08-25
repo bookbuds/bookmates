1e61a3e1cb08c4a9a298a6438c8aabfef0f12fb0
Matt J <unlrn@users.noreply.github.com>
2017-08-24 20:49:17 -0400

    added seeds

---
+++ seeders/User.js
@@ -0,0 +1,79 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert('Users', [
+        {
+            first_name: 'James',
+            last_name: 'Doe',
+            user_name: 'james',
+            profile_img_url: 'http://via.placeholder.com/150x200',
+            email: 'james@email.com',
+            password: '903482fds90uf239fjds0jds',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            first_name: 'James',
+            last_name: 'Doe',
+            user_name: 'james',
+            profile_img_url: 'http://via.placeholder.com/150x200',
+            email: 'james@email.com',
+            password: '903482fds90uf239fjds0jds',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            first_name: 'James',
+            last_name: 'Doe',
+            user_name: 'james',
+            profile_img_url: 'http://via.placeholder.com/150x200',
+            email: 'james@email.com',
+            password: '903482fds90uf239fjds0jds',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            first_name: 'James',
+            last_name: 'Doe',
+            user_name: 'james',
+            profile_img_url: 'http://via.placeholder.com/150x200',
+            email: 'james@email.com',
+            password: '903482fds90uf239fjds0jds',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+        {
+            first_name: 'James',
+            last_name: 'Doe',
+            user_name: 'james',
+            profile_img_url: 'http://via.placeholder.com/150x200',
+            email: 'james@email.com',
+            password: '903482fds90uf239fjds0jds',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        },
+    ])
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
