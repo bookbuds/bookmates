e1e9ca903103ddf0944385e273b439f9270b3220
Ryan Hardeman <Ryan Hardeman>
2017-08-26 12:58:42 -0400

    added for loop for json results

--- private/js/main.js
+++ private/js/main.js
@@ -11,7 +11,11 @@ var jason;
 var $ = require('jQuery');
 var parseString = require('xml2js').parseString;
 
+//api key
 var goodreadsKey = "vgXMPZsQvQc9pfKqedhA";
+
+//empty array for search results
+var searchResults = [];
 
 $(document).ready(function(){
 	var search = "Game of Thrones"
@@ -24,7 +28,7 @@ $(document).ready(function(){
 	}).done(function(response){
 			parseString(response, function(err, result){
 				console.log("====== JSON version ======")
-				console.log(result);
+				// console.log(result);
 
 				//console log book title
 				var bookTitle = result.GoodreadsResponse.search[0].results[0].work[0].best_book[0].title[0];
@@ -41,6 +45,19 @@ $(document).ready(function(){
 				console.log("====== IMG URL ======")
 				console.log(imgURL);
 
+				var results = result.GoodreadsResponse.search[0].results[0];
+
+					for (var i = 0; i< results.work.length; i++){
+						var work = results.work[i].best_book[0]
+
+						var tempObj ={
+							title:work.title[0],
+							author:work.author[0].name[0],
+							img: work.image_url[0]
+						}
+						searchResults.push(tempObj);
+					}
+					console.log(searchResults);
 			});
 	});
 });
