4fddba220db3476e1982cd9c7df2235c85c302af
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-08-30 11:54:10 -0400

    conversation now successfully created

--- controllers/messages/messages.js
+++ controllers/messages/messages.js
@@ -5,12 +5,16 @@ const db = require( '../../models' )
 //=========================
 // GET
 //=========================
-router.get( '/:conversationId', onGetMessages );
+router.get( '/:userId/:conversationId', onGetMessages );
 
 //GET CONVERSATIONS BETWEEN TWO USERS THAT ALREADY EXISTS
 function onGetMessages( tRequest, tResponse )
-{
+{  
+    //cache the conversation id
     const tempConversationId = tRequest.params.conversationId;
+
+    //cache the other user so we can send it back
+    const bookMateId = tRequest.params.userId;
 
     //if there is a user that's been auth'd
     if( tRequest.user )
@@ -34,19 +38,36 @@ function onGetMessages( tRequest, tResponse )
     //MIGHT HAVE TO GET THE COVO HERE
     function renderMessages( tMessages )
     {
-        for( let i = tMessages.length - 1; i >= 0; --i )
-        {
-            // if( tMessages[i].author )
-        }
-
         //console.log( JSON.stringify( tMessages, null, 2 ) );
-        tResponse.render( 'messages/messages', { messages: tMessages, user: tRequest.user } );
+        tResponse.render( 'messages/messages', { title: "Message", messages: tMessages, user: tRequest.user, bookMateId: bookMateId  } );
     }
 
     function sendJSON( tMessages )
     {
         const tempMessages = JSON.stringify( tMessages, null, 2 );
         tResponse.json( tempMessages );
+    }
+}
+
+router.get( '/:userId', onGetConversation );
+
+function onGetConversation( tRequest, tResponse )
+{
+    //current user making the request
+    const currentUserId = tRequest.user.id;
+    
+    //other user (that is being conversed with)
+    const bookMateId = parseInt( tRequest.params.userId );
+
+    console.log( currentUserId, bookMateId );
+
+    //probably should double check if it exists the opposite way?
+    //create Convo or get it if it exists
+    db.Conversation.findOrCreate( { where: { user1Id: currentUserId, user2Id: bookMateId } } ).then( onConversationCreated );
+
+    function onConversationCreated( tResults )
+    {
+        console.log( tResults );
     }
 }
 
